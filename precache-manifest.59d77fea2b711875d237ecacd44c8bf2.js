3c2d3b19f7cd45c5a4f4c7b7ba0113284af223dc
Luiz Fernando <lfrprazeres@gmail.com>
2020-10-03 17:30:17 -0300

    Updates

---
+++ precache-manifest.59d77fea2b711875d237ecacd44c8bf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1d408d82f8e0b89373b60cd4edbba3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e036b8b1b98caf7b2151",
+    "url": "/static/css/main.93fc025e.chunk.css"
+  },
+  {
+    "revision": "e7cf1b8586ba8e54b65f",
+    "url": "/static/js/2.5087fbb2.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.5087fbb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e036b8b1b98caf7b2151",
+    "url": "/static/js/main.32fda168.chunk.js"
+  },
+  {
+    "revision": "1b4a5f52ffeed69eede0",
+    "url": "/static/js/runtime-main.87678d3b.js"
+  },
+  {
+    "revision": "92235ad178248afa85df6515cd93920f",
+    "url": "/static/media/wave.92235ad1.svg"
+  }
+]);
