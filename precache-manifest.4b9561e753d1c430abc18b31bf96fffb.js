0135298a033555334dcace1fcce8fbc2fe010915
Luiz Fernando <lfrprazeres@gmail.com>
2020-10-03 23:44:03 -0300

    Updates

---
+++ precache-manifest.4b9561e753d1c430abc18b31bf96fffb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b68f28a9d187b9a7da267e540a25f27",
+    "url": "/Buscador-De-Palavras-2.0/index.html"
+  },
+  {
+    "revision": "c1e836e1799a5fa5351a",
+    "url": "/Buscador-De-Palavras-2.0/static/css/main.356514ff.chunk.css"
+  },
+  {
+    "revision": "6aeb0f19f53635d8ad0b",
+    "url": "/Buscador-De-Palavras-2.0/static/js/2.51a71f9b.chunk.js"
+  },
+  {
+    "revision": "b6a4d15389179fcf571735919b33e5b9",
+    "url": "/Buscador-De-Palavras-2.0/static/js/2.51a71f9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e836e1799a5fa5351a",
+    "url": "/Buscador-De-Palavras-2.0/static/js/main.b7b3db51.chunk.js"
+  },
+  {
+    "revision": "50ee97ba8af2de305f2e",
+    "url": "/Buscador-De-Palavras-2.0/static/js/runtime-main.b8242a7f.js"
+  },
+  {
+    "revision": "92235ad178248afa85df6515cd93920f",
+    "url": "/Buscador-De-Palavras-2.0/static/media/wave.92235ad1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0888af6cedf129a120e1262ff7e921b2.js"
+  "/Buscador-De-Palavras-2.0/precache-manifest.4b9561e753d1c430abc18b31bf96fffb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Buscador-De-Palavras-2.0/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
